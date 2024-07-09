de75f3fc97e659e0e78fc12c55fb4a1837e76cfd
ANVod <avodyasov@gmail.com>
2024-07-09 15:55:57 +0300

    add app

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,62 @@
-from flask import render_template, request, redirect, url_for
-from app import app
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from app import models, forms, db
+from app.models import User
+from app.forms import RegistrationForm, LoginForm
+import bcrypt
 
-posts = []
+app = Flask(__name__)
+login_manager = LoginManager(app)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    # Использует метод POST, так как информация будет отправляться. Request method сравнивает данные с HTTP-запросом.
-    if request.method == 'POST':
-        # Функция request.form извлекает значение из соответствующих полей
-        title = request.form.get('title')
-        content = request.form.get('content')
-        # Создает условие для проверки наличия данных в полях title и content
-        if title and content:
-            posts.append({'title': title, 'content': content})
-            # Использует для обновления страницы и предотвращения повторной отправки формы.
-            return redirect(url_for('index'))
-    # Возвращает отрендеренный шаблон с переданными данными постов
-    return render_template('blog.html', posts=posts)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.hashpw(form.password.data.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.checkpw(form.password.data.encode('utf-8'), user.password.encode('utf-8')):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Введены неверные данные', 'danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
